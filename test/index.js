0294e94763c42d3fd7020258274fce3c58094eba
Jad Salhani <salhanijad@gmail.com>
2016-04-03 14:29:21 +0300

    Update license reference + starting tests

---
+++ test/index.js
@@ -0,0 +1,11 @@
+var should = require('chai').should(),
+    ionicHandler = require('./src/core/ionicHandler'),
+    angularHandler = require('./src/core/angularHandler');
+
+beforeEach(() => {
+
+});
+
+describe('Ionic page generation') {
+
+}
