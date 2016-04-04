b7c1824ee6460f0ea8cbe39218b2dbeba71b2005
Jad Salhani <salhanijad@gmail.com>
2016-04-04 17:43:02 +0300

    Basic tests + testing travis

--- test/index.js
+++ test/index.js
@@ -1,11 +1,28 @@
 var should = require('chai').should(),
-    ionicHandler = require('./src/core/ionicHandler'),
-    angularHandler = require('./src/core/angularHandler');
+    ionicHandler = require(__dirname + '/../src/core/ionicHandler'),
+    //     angularHandler = require('./src/core/angularHandler');
 
-beforeEach(() => {
+    fsExtra = require('fs-sync');
+
+describe('Ionic page generation', function() {
+
+    beforeEach(function() {
+        fsExtra.mkdir(`${__dirname}/../app/pages/`);
+        fsExtra.mkdir(`${__dirname}/../app/components/`);
+    });
+
+    it('should create the page name file', function() {
+        fsExtra.mkdir(`${__dirname}/../app/pages/login/`);
+
+        ionicHandler.createPage('login', "./app/pages", "page");
+        var folderCreated = fsExtra.exists(`${__dirname}/../app/pages/login/`);
+
+        folderCreated.should.equal(true);
+    });
+
+
+    afterEach(function() {
+        fsExtra.remove(`${__dirname}/../app/`);
+    });
 
 });
-
-describe('Ionic page generation') {
-
-}
