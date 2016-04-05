68355ec96102df8808ff65051dedcd643df8f0aa
Jad Salhani <salhanijad@gmail.com>
2016-04-05 20:48:31 +0300

    Added component tests

--- test/index.js
+++ test/index.js
@@ -4,6 +4,7 @@ var should = require('chai').should(),
 /*eslint-enable*/
     base = process.env.PWD,
     ionicHandler = require(`${base}/src/core/ionichandler`),
+    angularHandler = require(`${base}/src/core/angularhandler`),
     fsExtra = require('fs-sync');
 
 describe('Ionic page generation', function() {
@@ -13,7 +14,7 @@ describe('Ionic page generation', function() {
         fsExtra.mkdir(`${base}/app/components/`);
     });
 
-    it('should create the page name file', function() {
+    it('should create the page_name files', function() {
         fsExtra.mkdir(`${base}/app/pages/login/`);
 
         ionicHandler.createPage('login', "./app/pages", "page");
@@ -22,6 +23,23 @@ describe('Ionic page generation', function() {
         folderCreated.should.equal(true);
     });
 
+    it('should create the component_name files', function() {
+        fsExtra.mkdir(`${base}/app/components/login/`);
+
+        angularHandler.createComponent('login', "./app/components");
+        var folderCreated = fsExtra.exists(`${base}/app/components/login/`);
+
+        folderCreated.should.equal(true);
+    });
+
+    it('should return false if the files already exist', function() {
+        fsExtra.mkdir(`${base}/app/components/login/`);
+        angularHandler.createComponent('login', "./app/components");
+
+        var attemptWithExistingFiles = angularHandler.createComponent('login', "./app/components");
+
+        attemptWithExistingFiles.should.equal(false);
+    });
 
     afterEach(function() {
         fsExtra.remove(`${base}/app/`);
